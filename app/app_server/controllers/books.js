b91dea3aed693a517f5a991da272e4971cc4ec4c
Andrew Rozendal <ascrozendal@gmail.com>
2017-11-06 11:10:11 -0800

    Finished Lab 8: moved all data to the controllers.  Added bookCard.pug and /includes to consolidate repeated book card generation to ensure consistency.

--- app/app_server/controllers/books.js
+++ app/app_server/controllers/books.js
@@ -1,11 +1,64 @@
 'use strict';
 
 const booksList = function(req, res){
-    res.render('index', { title: 'Rozendal Book Lists'});
-}
+    res.render('index', { 
+        title: 'Rozendal Book Lists',
+        pageHeader: {
+            title: 'Rozendal Book Lists',
+            tagline: 'Add a Book to Your Reading List',
+        },
+        sideContent: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi interdum non neque nec vulputate. Maecenas et sagittis nunc, ut accumsan ipsum. In eros sem, convallis rutrum interdum dignissim, viverra sit amet risus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec venenatis diam et nibh laoreet, id sagittis mi elementum. Maecenas eget ex sodales, luctus libero et, laoreet sapien.  Aenean dignissim euismod fringilla. Sed id maximus libero.',
+        books: [{
+            title: 'The Art of War',
+            coverImage: {
+                image: '/images/artOfWarCover.jpg',
+                altText: 'Art of War Book Cover'
+            }, 
+            catchphrase: 'A timeless classic on warfare',
+            author: 'Sun Tzu',
+            ISBN: '34252353-34234-234234',
+            rating: '5/5'
+        },
+        {
+            title: 'The Art of War',
+            coverImage: {
+                image: '/images/artOfWarCover.jpg',
+                altText: 'Art of War Book Cover'
+            }, 
+            catchphrase: 'A timeless classic on warfare',
+            author: 'Sun Tzu',
+            ISBN: '34252353-34234-234234',
+            rating: '5/5'
+        },
+        {
+            title: 'The Art of War',
+            coverImage: {
+                image: '/images/artOfWarCover.jpg',
+                altText: 'Art of War Book Cover'
+            }, 
+            catchphrase: 'A timeless classic on warfare',
+            author: 'Sun Tzu',
+            ISBN: '34252353-34234-234234',
+            rating: '5/5'
+        }]
+    });
+};
 
 const bookDetails = function(req, res){
-    res.render('book-details', { title: 'Book Details'});
+    res.render('book-details', { 
+        title: 'Book Details',
+        book: {
+            title: 'The Art of War',
+            coverImage: {
+                image: '/images/artOfWarCover.jpg',
+                altText: 'Art of War Book Cover'
+            }, 
+            catchphrase: 'A timeless classic on warfare',
+            author: 'Sun Tzu',
+            ISBN: '34252353-34234-234234',
+            rating: '5/5'
+        }
+    });
 }
 
 const addBookToLibrary = function(req, res){
@@ -17,7 +70,47 @@ const removeBookFromLibrary = function(req, res){
 }
 
 const readingList = function(req, res){
-    res.render('reading-list', { title: 'Your Reading List'});
+    res.render('reading-list', {
+        title: 'Your Reading List',
+        pageHeader: {
+            title: 'Your Reading List',
+            tagline: 'Manage Your Reading List',
+        },
+        sideContent: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi interdum non neque nec vulputate. Maecenas et sagittis nunc, ut accumsan ipsum. In eros sem, convallis rutrum interdum dignissim, viverra sit amet risus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec venenatis diam et nibh laoreet, id sagittis mi elementum. Maecenas eget ex sodales, luctus libero et, laoreet sapien.  Aenean dignissim euismod fringilla. Sed id maximus libero.',
+        books: [{
+            title: 'The Art of War',
+            coverImage: {
+                image: '/images/artOfWarCover.jpg',
+                altText: 'Art of War Book Cover'
+            }, 
+            catchphrase: 'A timeless classic on warfare',
+            author: 'Sun Tzu',
+            ISBN: '34252353-34234-234234',
+            rating: '5/5'
+        },
+        {
+            title: 'The Art of War',
+            coverImage: {
+                image: '/images/artOfWarCover.jpg',
+                altText: 'Art of War Book Cover'
+            }, 
+            catchphrase: 'A timeless classic on warfare',
+            author: 'Sun Tzu',
+            ISBN: '34252353-34234-234234',
+            rating: '5/5'
+        },
+        {
+            title: 'The Art of War',
+            coverImage: {
+                image: '/images/artOfWarCover.jpg',
+                altText: 'Art of War Book Cover'
+            }, 
+            catchphrase: 'A timeless classic on warfare',
+            author: 'Sun Tzu',
+            ISBN: '34252353-34234-234234',
+            rating: '5/5'
+        }]
+    });
 }
 
 module.exports = {
--- app/app_server/controllers/others.js
+++ app/app_server/controllers/others.js
@@ -1,7 +1,14 @@
 'use strict';
 
 const about = function(req, res) {
-  res.render('about', { title: 'About Rozendal Book Lists' });
+  res.render('about', { 
+    title: 'About Rozendal Book Lists',
+    aboutUs: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec dictum elementum sem. Cras nisi urna, scelerisque quis pretium sit amet, dictum vitae quam. Fusce ultrices vel massa a sagittis. Vivamus egestas, ipsum id eleifend consequat, turpis arcu tincidunt sapien, in maximus sem erat eu augue. Sed egestas non lectus ut semper. Fusce nec faucibus massa, et egestas justo. Morbi ullamcorper erat nec egestas ullamcorper.  Integer enim justo, eleifend at fringilla sed, elementum porta lorem. Phasellus a commodo neque. Aliquam ut ante nisl. Proin consectetur lacinia pharetra. Suspendisse pulvinar, lacus ut dapibus aliquet, dolor ante congue sem, pretium porta diam neque id metus. Pellentesque nec turpis metus. Curabitur varius tempus justo a pulvinar. Ut nec tellus pharetra magna molestie vestibulum ac vel neque. Curabitur id interdum risus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nullam sed ex justo. Praesent mauris arcu, porttitor sit amet iaculis et, egestas vestibulum lorem. Nulla varius eleifend metus, eget ultrices ex. Nulla ac viverra enim, ut vehicula tortor. Nunc sit amet aliquet nulla, quis facilisis massa. Nullam tempor euismod feugiat.',
+    image: {
+      image: '/aboutUs.jpg',
+      altText: 'Picture goes here'
+    }
+  });
 };
 
 module.exports = {
