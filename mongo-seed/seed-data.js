d2acea346ed44990dd3dbb80018eed0bdda6e09e
Andrew Rozendal <ascrozendal@gmail.com>
2017-11-20 09:59:28 -0800

    added mongo-seed dir and seed-data.js to init database with data.  Added corresponding images for books

---
+++ mongo-seed/seed-data.js
@@ -0,0 +1,51 @@
+db.books.save({
+    title: 'The Art of War',
+    coverImage: {
+        _id: ObjectId(),
+        image: '/images/artOfWar.jpg',
+        altText: 'Art of War Book Cover'
+    }, 
+    catchphrase: 'A timeless classic on warfare',
+    author: 'Sun Tzu',
+    ISBN: '0981162614',
+    rating: '5/5'
+});
+
+db.books.save({
+    title: 'The Iliad',
+    coverImage: {
+        _id: ObjectId(),
+        image: '/images/theIliad.jpg',
+        altText: 'The classic poem by Homer'
+    }, 
+    catchphrase: 'Tells the story of the Trojan War',
+    author: 'Homer trans. Robert Fagles',
+    ISBN: '0140275363',
+    rating: '5/5'
+});
+
+db.books.save({
+    title: 'War and Peace',
+    coverImage: {
+        _id: ObjectId(),
+        image: '/images/warAndPeace.jpg',
+        altText: 'War and Peace Book Cover'
+    }, 
+    catchphrase: 'Tolstoy\'s masterwork on Napoleon\s invasion of Russia in 1812',
+    author: 'Leo Tolstoy trans. Richard Peaver, Larissa Volokhonsky',
+    ISBN: '1400079985',
+    rating: '5/5'
+});
+
+db.books.save({
+    title: 'Nineteen Eighty-four',
+    coverImage: {
+        _id: ObjectId(),
+        image: '/images/nineteenEightyFour.jpg',
+        altText: 'Nineteen Eighty-Four Book Cover'
+    }, 
+    catchphrase: 'A Dystopian future where War is Peace and Big Brother reigns supreme',
+    author: 'George Orwell',
+    ISBN: '1788282361',
+    rating: '5/5'
+});
