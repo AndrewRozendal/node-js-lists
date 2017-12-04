6dfda69acd2b8b070f4c5ac158eaf79d7d829d1a
Andrew Rozendal <ascrozendal@gmail.com>
2017-12-04 11:46:01 -0800

    fixed images not displaying: needed to reference subdocument returned from api layer as book.coverImage[0].image

--- mongo-seed/seed-data.js
+++ mongo-seed/seed-data.js
@@ -2,39 +2,39 @@ db.books.save({
     title: 'The Art of War',
     coverImage: {
         _id: ObjectId(),
-        image: 'images/artOfWar.jpg',
+        image: '/images/artOfWar.jpg',
         altText: 'Art of War Book Cover'
     }, 
     catchphrase: 'A timeless classic on warfare',
     author: 'Sun Tzu',
     ISBN: '0981162614',
-    rating: '5'
+    rating: 5
 });
 
 db.books.save({
     title: 'The Iliad',
     coverImage: {
         _id: ObjectId(),
-        image: 'images/theIliad.jpg',
+        image: '/images/theIliad.jpg',
         altText: 'The classic poem by Homer'
     }, 
     catchphrase: 'Tells the story of the Trojan War',
     author: 'Homer trans. Robert Fagles',
     ISBN: '0140275363',
-    rating: '5'
+    rating: 5
 });
 
 db.books.save({
     title: 'War and Peace',
     coverImage: {
         _id: ObjectId(),
-        image: 'images/warAndPeace.jpg',
+        image: '/images/warAndPeace.jpg',
         altText: 'War and Peace Book Cover'
     }, 
     catchphrase: 'Tolstoy\'s masterwork on Napoleon\s invasion of Russia in 1812',
     author: 'Leo Tolstoy trans. Richard Peaver, Larissa Volokhonsky',
     ISBN: '1400079985',
-    rating: '5',
+    rating: 5,
     detailedDescription: 'This should not appear on the home page'
 });
 
@@ -42,13 +42,13 @@ db.books.save({
     title: 'Nineteen Eighty-four',
     coverImage: {
         _id: ObjectId(),
-        image: 'images/nineteenEightyFour.jpg',
+        image: '/images/nineteenEightyFour.jpg',
         altText: 'Nineteen Eighty-Four Book Cover'
     }, 
     catchphrase: 'A Dystopian future where War is Peace and Big Brother reigns supreme',
     author: 'George Orwell',
     ISBN: '1788282361',
-    rating: '5'
+    rating: 5
 });
 
 db.users.save({
